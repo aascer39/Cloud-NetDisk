a732dfecdf6b676cd7fbbbddbced469399ea1ac6
fufu39 <141384958+aascer39@users.noreply.github.com>
2025-05-29 01:49:48 +0800

    chore: 添加 NetDisk 项目配置文件
    - 新增 .idea 目录下的多个配置文件，包括 gitignore、compiler、dataSources 等
    - 添加 Network backend.iml 模块文件
    - 新增 MyBatisX 和 inspection Profiles 配置
    - 设置项目编码和 JVM 目标版本

---
+++ NetDisk/src/test/java/com/zjj/netdisk/NetDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjj.netdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetDiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
